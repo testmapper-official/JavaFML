72921fd2fba99da147950700763076ea03977f8c
testmapper <testmapper@yandex.ru>
2022-03-17 21:16:34 +0300

    #8 BottomNavigationDrawerFragment completed

--- app/src/main/java/com/example/fmli_app/Activity/MainActivity.java
+++ app/src/main/java/com/example/fmli_app/Activity/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.fmli_app.Activity;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
@@ -8,26 +13,22 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
+import com.example.fmli_app.Fragment.BottomNavigationDrawerFragment;
 import com.example.fmli_app.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
     AppBarConfiguration navBarConfiguration;
     BottomNavigationView navbar;
+    NavigationView navdrawer;
     NavController navcontroller;
     NavHostFragment navhost;
     FragmentManager fragmentManager;
 
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         // Отключение заголовка
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         // Отключение панели действий
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
@@ -46,7 +47,18 @@ public class MainActivity extends AppCompatActivity {
 
         // Установка нижней панели навигации
         navbar = findViewById(R.id.bottom_navigation);
-        navcontroller = Navigation.findNavController(this,  R.id.maingraph);
+        navcontroller = Navigation.findNavController(this, R.id.maingraph);
         NavigationUI.setupWithNavController(navbar, navcontroller);
+
+
+        navbar.setOnItemSelectedListener((menuItem) -> {
+            if (menuItem.getItemId() == R.id.page_add) {
+                BottomNavigationDrawerFragment navigationView = new BottomNavigationDrawerFragment();
+                navigationView.show(getSupportFragmentManager(), navigationView.getTag());
+            }
+            return true;
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/fmli_app/Fragment/BottomNavigationDrawerFragment.java
@@ -0,0 +1,81 @@
+package com.example.fmli_app.Fragment;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import com.example.fmli_app.R;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_bottomsheet, container, false);
+
+        NavigationView nav_view = view.findViewById(R.id.navigation_drawer);
+
+        nav_view.setNavigationItemSelectedListener((menuItem) -> {
+            switch (menuItem.getItemId()) {
+                case R.id.app_bar_add_comm:
+                    //
+                case R.id.app_bar_add_news:
+                    //
+                case R.id.app_bar_add_tag:
+                    //
+                case R.id.app_bar_settings:
+                    //
+            }
+            return true;
+        });
+
+        return view;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void setWhiteNavigationBar(@NonNull Dialog dialog) {
+        Window window = dialog.getWindow();
+        if (window != null) {
+            DisplayMetrics metrics = new DisplayMetrics();
+            window.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+            GradientDrawable dimDrawable = new GradientDrawable();
+            // ...customize your dim effect here
+
+            GradientDrawable navigationBarDrawable = new GradientDrawable();
+            navigationBarDrawable.setShape(GradientDrawable.RECTANGLE);
+            navigationBarDrawable.setColor(Color.WHITE);
+
+            Drawable[] layers = {dimDrawable, navigationBarDrawable};
+
+            LayerDrawable windowBackground = new LayerDrawable(layers);
+            windowBackground.setLayerInsetTop(1, metrics.heightPixels);
+
+            window.setBackgroundDrawable(windowBackground);
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setWhiteNavigationBar(dialog);
+        }
+
+        return dialog;
+    }
+}
